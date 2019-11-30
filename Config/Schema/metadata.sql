89718fe9c1425e6328ba84f8a2df687f46dad02a
heron03 <heronze@gmail.com>
2019-11-30 00:51:07 -0300

    Configuração

---
+++ Config/Schema/metadata.sql
@@ -0,0 +1,111 @@
+DROP TABLE IF EXISTS `escolas`;
+CREATE TABLE `escolas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `blocked` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `cursos`;
+CREATE TABLE `cursos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  `semestres` int(2) DEFAULT NULL,
+  `escola_id` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `blocked` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `cursos_escola_id_fk` FOREIGN KEY (`escola_id`) REFERENCES `escolas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  `login` varchar(100) DEFAULT NULL,
+  `senha` varchar(255) DEFAULT NULL,
+  `escola_id` int(11) DEFAULT NULL,
+  `cpf` varchar(20) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `aro_parent_id` int(11) DEFAULT NULL,
+  `blocked` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `usuarios_escola_id_fk` FOREIGN KEY (`escola_id`) REFERENCES `escolas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `turmas`;
+CREATE TABLE `turmas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  `semestres` int(2) DEFAULT NULL,
+  `curso_id` int(11) DEFAULT NULL,
+  `usuarios_id` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `blocked` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `turmas_curso_id_fk` FOREIGN KEY (`curso_id`) REFERENCES `cursos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `turmas_usuarios_id_fk` FOREIGN KEY (`usuarios_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `diciplinas`;
+CREATE TABLE `diciplinas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  `turma_id` int(11) DEFAULT NULL,
+  `usuarios_id` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `blocked` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `diciplinas_turma_id_fk` FOREIGN KEY (`turma_id`) REFERENCES `turmas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `diciplinas_usuarios_id_fk` FOREIGN KEY (`usuarios_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `aulas`;
+CREATE TABLE `aulas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  `diciplina_id` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `blocked` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `aulas_diciplina_id_fk` FOREIGN KEY (`diciplina_id`) REFERENCES `diciplinas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `provas`;
+CREATE TABLE `provas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  `diciplina_id` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `blocked` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `provas_diciplina_id_fk` FOREIGN KEY (`diciplina_id`) REFERENCES `diciplinas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `exercicios`;
+CREATE TABLE `exercicios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  `prova_id` int(11) DEFAULT NULL,
+  `aula_id` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `blocked` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `exercicios_aula_id_fk` FOREIGN KEY (`aula_id`) REFERENCES `aulas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `exercicios_prova_id_fk` FOREIGN KEY (`prova_id`) REFERENCES `provas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
