a1167823ed79fc3cf5ea3fe30c845981bbd693e6
heron03 <heronze@gmail.com>
2019-11-30 20:24:28 -0300

    Validação

--- Model/Disciplina.php
+++ Model/Disciplina.php
@@ -10,5 +10,15 @@ class Disciplina extends AppModel {
         'Usuario',
         'Turma'
     );
+
+    public $validate = array(
+        'data' => array(
+            'notBlank' => array('rule' => 'notBlank', 'message' => 'Campo Obrigatório'),
+            'dataAtual' => array('rule' => 'vencimentoIgualDataAtual', 'message' => 'Não pode ser igual a data atual'),
+        ),
+        'nome' => array(
+            'notBlank' => array('rule' => 'notBlank', 'message' => 'Campo Obrigatório')
+        )
+    );
 }
 ?>
